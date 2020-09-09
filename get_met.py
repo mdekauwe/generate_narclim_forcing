56ac0ad3464cece3cc44ec203583f69a4d1d9693
mdekauwe <mdekauwe@gmail.com>
2020-09-09 13:50:28 +1000

    count tracker

--- get_met.py
+++ get_met.py
@@ -136,7 +136,7 @@ def main(path, slice, GCM, RCM, domain, opath, spp, lat, lon, df_co2):
     df_out['date'] = pd.to_datetime(df_out.index)
     cols = ['date','tas','huss','pracc','wss','ps','rlds','rsds']
     df_out = df_out[cols]
-    df_out.rename(columns={'tas':'Tair', 'huss':'Qair', 'pracc':'Precip',
+    df_out.rename(columns={'tas':'Tair', 'huss':'Qair', 'pracc':'Rainf',
                            'wss':'Wind', 'ps':'Psurf', 'rlds':'LWdown',
                            'rsds':'SWdown'},
                   inplace=True)
