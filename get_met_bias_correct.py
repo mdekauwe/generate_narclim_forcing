555bd95ad235db8f54d39501f0b8a437a15ee9a1
mdekauwe <mdekauwe@gmail.com>
2020-11-10 09:13:12 +1100

    added bias correction script

--- get_met_bias_correct.py
+++ get_met_bias_correct.py
@@ -36,11 +36,13 @@ def main(path, bias_path, slice, GCM, RCM, domain, opath, spp, lat, lon, df_co2,
         dsx = xr.open_dataset(fn)
         lats = dsx.lat[:,0].values # 2D arrays, squeeze
         lons = dsx.lon[0,:].values # 2D arrays, squeeze
-        print(lats)
-        print(lons)
+
         print(lat, lon)
         ii = find_nearest(lats, lat)
         jj = find_nearest(lons, lon)
+        print(ii, jj)
+        print(dsx['lat'][ii,jj])
+        print(dsx['lon'][ii,jj])
         data = dsx['pracc_bc'][:,ii,jj].to_dataframe()
         data = data.drop(['lat', 'lon'], axis=1)
         print(np.nanmean(data.pracc_bc))
